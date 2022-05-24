64b6335de7d14f8656c4afc2dbb545e815d0c988
Kratos40-sba <a.khebchi@esi-sba.dz>
2022-05-24 06:25:53 +0100

    more analysis tests added

--- src/lib/analysis.rs
+++ src/lib/analysis.rs
@@ -1,17 +1,32 @@
 use std::collections::HashMap;
+use std::fmt::format;
+use image::{GrayImage, ImageFormat};
 use rand::{Rng,rngs::ThreadRng};
-use crate::algo::{decrypt_image, encrypt_image};
+use crate::algo::encrypt_image;
 use crate::image_utils::{compose_image, decompose};
+use crate::init::dkv;
 
 fn image_to_array(path : &str) -> Vec<u8> {
     image::open(path).unwrap()
         .to_luma8().to_vec()
 }
+fn image_to_matrix(path : &str) -> [[u8;256];256] {
+    let mut matrix = [[0u8;256];256] ;
+    let image = image::open(
+     format!("{}.bmp",path).as_mut_str()
+    ).unwrap().to_luma8() ;
+    let (w,h) = image.dimensions() ;
+    for i in 0..w {
+        for j in 0..h {
+            matrix[i as usize][j as usize] = image.get_pixel(i,j).0[0]
+        }}
+    matrix
+}
 fn histogram_vals(path : &str) -> Vec<i32> {
     let encrypted_image : Vec<u8> = image_to_array(path) ;
     let mut histo : HashMap<u8,i32> = HashMap::new() ;
     for i in 0..255 {
-        histo.insert(i,0)
+        histo.insert(i,0);
     }
     for i in 0..encrypted_image.len() {
         if histo.contains_key(&encrypted_image[i]) {
@@ -72,13 +87,114 @@ pub fn entropy_analysis(path : &str) -> f32 {
                 decompose(path).0 , dkv ) ,
             decompose(path).1 , decompose(path).2
                 , "encrypted"
-            )
+            );
         let entropy_test = entropy_test("encrypted.bmp");
         arr_result[i] = entropy_test ;
         mean += arr_result[i]
     }
     mean/100.0
 }
+fn random_pixel_change(path : &str) {
+    let mut image_matrix = image_to_matrix(path) ;
+    let mut rnd = rand::thread_rng();
+    let index_pix: u8 = rnd.gen() ;
+    image_matrix[index_pix  as usize][index_pix  as usize ]  -= 1 ;
+    let mut image_to_write = GrayImage::new(256, 256) ;
+    for x in 0..256 {
+        for y in 0..256 {
+            image_to_write.put_pixel(
+                x,
+                y,
+                image::Luma(image_matrix[x as usize][y as usize].to_be_bytes())
+            );
+        }
+    }
+    image_to_write.save_with_format(format!("modified_{}.bmp",path), ImageFormat::Bmp).expect("error while saving the image");
+}
+fn hamming_distance (x : i32, y : i32) -> i32 {
+    let mut counter : i32 = 0; 
+    let mut z = x ^ y ;
+    while z != 0  {
+        counter += z & 1;
+        z >>= 1;
+    }
+    counter
+    
+}
+
+pub fn ks_ps (c1 : &str , c2 : &str) -> f32 {
+    let mut ks = 0f32;
+    let c1_matrix = image_to_array(c1);
+    let c2_matrix = image_to_array(c2);
+    let factor : f32 = (100.00/((c2_matrix.len() as f32 ) * 8.0)) ;
+    for i in 0..c1_matrix.len() {
+        ks += (hamming_distance(c1_matrix[i] as i32 , c2_matrix[i] as i32)) as f32
+    }
+    ks * factor
+}
+fn npcr_uaci (c1 : &str , c2 : &str) -> (f32,f32){
+    let factor = (100.0/(f32::powi(255.0,2)));
+    let (mut npcr,mut uaci ) = (0f32, 0f32) ;
+    let c1_matrix = image_to_matrix(format!("{}",c1).as_mut_str());
+    let c2_matrix = image_to_matrix(format!("{}",c2).as_mut_str());
+    for i in 0..256 {
+        for j in 0..256 {
+            if c1_matrix[i][j] != c2_matrix[i][j] {
+                npcr += 1.0 ;
+            }
+            uaci += (f32::abs(c1_matrix[i][j] as f32 - c2_matrix[i][j] as f32))/ 255.0
+        }
+    }
+    (npcr * factor ,uaci * factor)
+}
+pub fn robustness_analysis (image_name : &str) -> (f32 , f32 , f32) {
+    let (mut npcr_avg , mut uaci_avg, mut ps_avg) = (0f32,0f32,0f32) ;
+    for i in 0..100 {
+        random_pixel_change(image_name);
+        let m_k : [u8;8] = [101,20,30,40,50,60,128,99];
+        let dkv = dkv(&m_k) ;
+        let (blocks,w,h) = decompose(
+            format!("modified_{}.bmp",image_name).as_mut_str()
+        );
+        let encrypted_blocks = encrypt_image(blocks,dkv);
+        compose_image(encrypted_blocks.clone(),w,h,
+                      format!("modified_encrypted_{}",image_name).as_mut_str());
+        let (npcr,uaci) = npcr_uaci(
+            format!("encrypted_{}",image_name).as_mut_str() ,
+            format!("modified_encrypted_{}",image_name).as_mut_str()) ;
+        let ps = ks_ps(
+            format!("encrypted_{}.bmp",image_name).as_mut_str() ,
+            format!("modified_encrypted_{}.bmp",image_name).as_mut_str()
+        );
+        println!("npcr = {} uaci = {} PS= {}",npcr , uaci , ps);
+        npcr_avg += npcr ;
+        uaci_avg += uaci ;
+        ps_avg += ps
+
+    }
+    (npcr_avg/100.0 , uaci_avg/100.0 ,ps_avg/100.0 )
+}
+
+
 // correlation
 // choose N random pixels
-// vertical x = m[i][j] / y = m[i][j-1] or m[i][j+1]
+/*
+ Matlab code :
+ lena = double(imread('lena.bmp'))
+ // horizontal
+ x =  lena(:,1:end-1)
+ y =  lena(:,2:end)
+ rxy = corr2(x,y) -> h_corr value = 0.9430 /  0.0045
+ scatter(x(:),y(:)) -> plot
+ // vertical
+ x =  lena(1:end-1,:)
+ y =  lena(2:end,:)
+ rxy = corr2(x,y) -> v_corr value =  0.9712 / -0.0025
+ scatter(x(:),y(:)) -> plot
+ // diagonal
+ x = lena(1:end-1,1:end-1)
+ y = lena(2:end,2:end)
+ rxy = corr2(x,y) -> d_corr value = 0.9203 /   9.9148e-04
+
+ scatter(x(:),y(:)) -> plot
+ */
--- src/test.rs
+++ src/test.rs
@@ -1,13 +1,61 @@
-use img_enc::image_utils::* ;
-use img_enc::init::dkv;
 use img_enc::algo::{decrypt_image, encrypt_image};
+use img_enc::analysis::{ ks_ps, robustness_analysis};
+use img_enc::image_utils::{compose_image, decompose};
+use img_enc::init::dkv;
+
 fn main() {
-    let m_k : [u8;8] = [101,20,30,40,50,60,128,99];
+  let (npcr,uaci,ps) = robustness_analysis("lena");
+  println!("NPCR = {} \n UACI = {}\n PS = {}",npcr , uaci , ps);
+
+
+}
+/*
+ let m_k : [u8;8] = [101,20,30,40,50,60,128,99];
     let dkv = dkv(&m_k) ;
     let (blocks,w,h) = decompose("Lena_512_rust.bmp");
     let encrypted_blocks = encrypt_image(blocks,dkv);
     compose_image(encrypted_blocks.clone(),w,h,"decrypted_lena");
     let decrypted_blocks = decrypt_image(encrypted_blocks,dkv);
     compose_image(decrypted_blocks,w,h,"encrypted_lena")
+ */
+/*
+    let uniformity_analysis = uniformity_analysis("lena.bmp");
+    println!("mean in uniformity analysis is = {}",uniformity_analysis);
+    // 252.70805
+    let entropy_analysis = entropy_analysis("lena.bmp");
+    println!("mean in entropy analysis is = {}",entropy_analysis);
+    // 7.991547
+ */
+/*
+ random_pixel_change("lena.bmp");
+    let m_k : [u8;8] = [101,20,30,40,50,60,128,99];
+    let dkv = dkv(&m_k) ;
+    let (blocks,w,h) = decompose("modified_lena.bmp");
+    let encrypted_blocks = encrypt_image(blocks,dkv);
+    compose_image(encrypted_blocks.clone(),w,h,"modified_encrypted_lena");
 
-}
+ */
+/*
+    let (npcr , uaci ) = npcr_uaci("encrypted_lena.bmp","modified_encrypted_lena.bmp");
+    println!("NPCR = {} \nUACI = {} " , npcr , uaci);
+
+      let dkv1:[u8;64] = [160, 56, 20, 77, 161, 56, 20, 77, 49, 229, 205, 52, 49, 230,
+        205, 52, 165, 235, 219, 77, 165, 235, 220, 77, 138, 218, 83,
+        209, 138, 218, 83, 210, 159, 56, 20, 77, 160, 56, 20, 77, 49,
+        228, 205, 52, 49, 229, 205, 52, 165, 235, 218, 77, 165, 235,
+        219, 77, 138, 218, 83, 208, 138, 218, 83, 209];
+    let dkv2 :[u8;64] = [160, 56, 20, 77, 161, 56, 20, 77, 49, 229, 205, 52, 49, 230,
+    205, 52, 165, 235, 219, 77, 165, 235, 220, 77, 138, 218, 83,
+    209, 138, 218, 83, 210, 159, 56, 20, 77, 160, 56, 20, 77, 49,
+    228, 205, 52, 49, 229, 205, 52, 165, 235, 218, 77, 165, 235,
+    219, 77, 138, 218, 83, 208, 138, 218, 83, 210] ;
+    let (blocks,w,h) = decompose("lena.bmp");
+    let encrypted_blocks = encrypt_image(blocks.clone(),dkv1);
+    let encrypted_blocks2 = encrypt_image(blocks,dkv2);
+    compose_image(encrypted_blocks,w,h,"c_k1");
+    compose_image(encrypted_blocks2,w,h,"c_k2");
+    let ks = ks_ps("c_k1.bmp","c_k2.bmp");
+    println!("ks = {} \n",ks);
+    let ps = ks_ps("encrypted_lena.bmp","modified_encrypted_lena.bmp");
+    println!("PS = {} \n",ps);
+ */
